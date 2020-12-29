49d6308d77b0811d7091244eeac362c7553eb398
Tim Fielder <fiel1139@gmail.com>
2020-12-28 20:16:35 -0700

    add additional views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'dashboards#show'
+  root to: 'welcome#home'
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
