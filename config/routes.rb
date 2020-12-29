d77de2fe0b75dc5ba1a3850daf4eb9dfcd96fb8a
Tim Fielder <fiel1139@gmail.com>
2020-12-28 19:57:54 -0700

    add controllers and additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/dashboard', to: 'users#dashboard'
+
   resources :users, only: [:new, :create, :edit, :update] do
     resources :workouts do
       resources :exercises
