0b36d936e29f1c3aa339b9a344910f6678e7bc98
Tim Fielder <fiel1139@gmail.com>
2020-12-28 20:27:05 -0700

    update symbol

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :workouts, dependent: :destroy
-  has_many :exercises, through :workouts
+  has_many :exercises, through: :workouts
 
   has_secure_password
 
