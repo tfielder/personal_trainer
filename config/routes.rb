be25cf43327cc47cde2546526fad06740eb52d8c
Tim Fielder <fiel1139@gmail.com>
2020-12-28 20:49:01 -0700

    tweak dashboard route and view

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get '/dashboard', to: 'users#dashboard'
+  get '/dashboard', to: 'dashboards#show'
 
   resources :users, only: [:new, :create, :edit, :update] do
     resources :workouts do
