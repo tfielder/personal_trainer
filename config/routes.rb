99fd669b40b1f30e77412bc1bef2aec062b6d118
Tim Fielder <fiel1139@gmail.com>
2020-12-30 10:23:50 -0700

    add images, styling, routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get '/dashboard', to: 'dashboards#show'
+  get '/dashboard', to: 'dashboards#index'
 
   get '/workouts', to: 'workouts#index'
   get '/community', to: 'community#index'
