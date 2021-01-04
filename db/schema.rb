e796e2bbb4ca7def83581b50ec09188c2776c6e3
Tim Fielder <fiel1139@gmail.com>
2021-01-03 18:40:20 -0700

    add migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_012030) do
+ActiveRecord::Schema.define(version: 2021_01_04_013318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_012030) do
     t.bigint "exercise_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "duration"
     t.index ["exercise_id"], name: "index_setlists_on_exercise_id"
   end
 
